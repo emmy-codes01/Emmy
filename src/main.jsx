bb58af41f1442033ead4d4859f9b46dc8b25979b
emmy-codes01 <eayeni185@gmail.com>
2026-08-29 01:15:37 +0100

    fix react helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Helmet } from "react-helmet-async";
+// import { Helmet } from "react-helmet-async";
 import './index.css'
 import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
+    {/* <HelmetProvider> */}
     <App />
-    </HelmetProvider>
+    {/* </HelmetProvider> */}
   </StrictMode>,
 )
